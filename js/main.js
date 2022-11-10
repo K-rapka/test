e02238ff5149ea5ab6db8abb94003ccc6e90b974
alexandr <sasha.drupp24@gmail.com>
2022-11-10 19:15:36 +0200

    0.2_beta

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-let = input = 10*2-(2+1)
-console.log(Number(input));
+let theme = document.querySelector('.calc-theme')
+let currentTheme = 'light'
+theme.addEventListener('click', ()=>{ 
+    if (currentTheme == 'light'){
+        currentTheme = 'dark'
+        theme.classList = 'calc-theme dark'
+    }
+    else{
+        currentTheme = 'light'
+        theme.classList = 'calc-theme light'
+    }
+ })
